b396b6058d4aed609516ca6162db8f1570d717ca
rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-08-27 11:19:22 -0500

    modified:   Unit 1/Greeting App/Greeting App/ContentView.swift

--- Unit 1/Greeting App/Greeting App/ContentView.swift
+++ Unit 1/Greeting App/Greeting App/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var greeting = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Greeting App")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
+                .padding()
+            
+            TextField("Enter Name", text: $name)
+                .multilineTextAlignment(.center)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .padding()
+            Button("Say Hello"){
+                greeting = "Hello, \(name)!"
+            }
+            .padding()
+            
         }
-        .padding()
     }
 }
 
