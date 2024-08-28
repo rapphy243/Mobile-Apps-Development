fbba926b2279121617205c166ebf3126741f7525
rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-08-28 09:53:58 -0500

    Add multiply

---
+++ Unit 1/Multiply/Multiply/MultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyApp.swift
+//  Multiply
+//
+//  Created by Raphael Abano on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
