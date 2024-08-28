c48a4520fe70379cd9ff0cb0a1ec3e5456c2e2ad
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-08-27 21:28:47 -0500

    Finished Greeting App

--- Unit 1/Greeting App/Greeting App/ContentView.swift
+++ Unit 1/Greeting App/Greeting App/ContentView.swift
@@ -9,22 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var name = ""
-    @State private var greeting = ""
+    @State private var greeting = "Hello, [name]!"
     var body: some View {
         VStack {
             Text("Greeting App")
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
                 .padding()
-            
+            Text(greeting)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .padding()
             TextField("Enter Name", text: $name)
                 .multilineTextAlignment(.center)
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 .padding()
-            Button("Say Hello"){
-                greeting = "Hello, \(name)!"
+            HStack {
+                Button("Say Hello"){
+                    greeting = "Hello, \(name)!"
+                }
+                .padding()
+                Button("Say Goodbye"){
+                    greeting = "Goodbye, \(name)!"
+                }
+                .padding()
+                Button("Say Aloha"){
+                    greeting = "Aloha, \(name)!"
+                }
+                .padding()
             }
-            .padding()
-            
         }
     }
 }
