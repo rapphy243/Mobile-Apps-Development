1b6b874606df2c58d865919372544518c5b02d75
rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-09-15 00:53:20 -0500

    Update Scrumdinger

---
+++ Apple Projects/Scrumdinger/Scrumdinger/Models/ScrumsView.swift
@@ -0,0 +1,8 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Raphael Abano on 9/13/24.
+//
+
+import Foundation
---
+++ Apple Projects/Scrumdinger/Scrumdinger/Models/TrailingIconLabelStyle.swift
@@ -0,0 +1,8 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Raphael Abano on 9/13/24.
+//
+
+import Foundation
