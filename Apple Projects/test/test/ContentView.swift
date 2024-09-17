787ae0d1e70573838cec4ac511ca86762fe33ff4
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-09-16 19:09:24 -0500

    Fix stuffs

---
+++ Apple Projects/test/test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Raphael Abano on 9/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello,!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Apple Projects/test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Raphael Abano on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
