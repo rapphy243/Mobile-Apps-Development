70e1dd4f34ecccdbf02a4bfcbcd34630d01cefeb
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-09-24 09:34:15 -0500

    Finish Madlib

--- Unit 3/Madlibs/Madlibs/storyView.swift
+++ Unit 3/Madlibs/Madlibs/storyView.swift
@@ -8,16 +8,25 @@
 import SwiftUI
 
 struct storyView: View {
-    @State var word: words
+    let word : words
     var body: some View {
-        Text(writeStory())
+        NavigationView {
+            VStack {
+                Text(writeStory())
+                    .frame(width: 350, height: 1100)
+            }
+            .navigationTitle("Story View")
+        }
+    }
+    func writeStory() -> String {
+        
+        var story = "I walk through the \(word.color) jungle. I take out my \(word.adjective0) canteen. " + "There's a \(word.adjective1) parrot with a \(word.adjective2) \(word.noun0) in his mouth right there in front of me in the \(word.adjective3) trees! "
+        story = story + "I gaze at his \(word.adjective2) \(word.noun0). " + "A sudden sound awakes me from my daydream! A panther is \(word.verb0) in front of me! " + "I smell his \(word.adjective4) breath. " + "I remember I have a packet of \(word.noun1) that makes it go into a deep slumber! " + "I \(word.verb1) it away from me in front of the panther." + " Yes he's eating it! I \(word.verb2) away through the \(word.color) jungle. I meet my parents at the tent. Phew! It’s been an exciting day in the jungle."
+        return story
     }
 }
 struct storyViews_Previews: PreviewProvider {
     static var previews: some View {
-        storyView(word: words())
+        storyView(word : words())
     }
-}
-func writeStory() -> String {
-    return "I walk through the color jungle. I take out my \(word.adjective0) canteen. " + "There's a \(word.adjective1) parrot with a \(word.adjective2) (noun) in his mouth right there in front of me in the (adjective) trees!"
 }
