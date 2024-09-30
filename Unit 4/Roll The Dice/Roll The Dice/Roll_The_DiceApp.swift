eba0016dab08b5ece3b569e1c6ebfe49ced97275
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-09-30 09:44:06 -0500

    Upload Roll The Dice

---
+++ Unit 4/Roll The Dice/Roll The Dice/Roll_The_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_The_DiceApp.swift
+//  Roll The Dice
+//
+//  Created by Raphael Abano on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Roll_The_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
