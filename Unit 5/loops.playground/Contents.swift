1767ee93b391797a83ee7eff4a2de14940a152eb
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-10-08 11:08:42 -0500

    modified:   Unit 3/Madlibs 	new file:   Unit 5/Arrays Challenge 	modified:   Unit 5/Arrays.playground/Contents.swift 	modified:   Unit 5/Arrays.playground/contents.xcplayground 	new file:   Unit 5/Loops Challenge 	modified:   Unit 5/loops.playground/Contents.swift 	modified:   Unit 5/loops.playground/contents.xcplayground 	modified:   Unit 5/Arrays Challenge (modified content, untracked content) 	modified:   Unit 5/Loops Challenge (modified content, untracked content)

--- Unit 5/loops.playground/Contents.swift
+++ Unit 5/loops.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
-
-var greeting = "Hello, playground"
+// Loops hackwich
+var stupidIdeas = ["Play tag on highway", "Shout \"Fire\" in a crowded room",
+"Forget your school ID", "etc."]
+for stupidIdea in stupidIdeas {
+ print("Don't ever do the following: \(stupidIdea)")
+}
+for number in 1...100 {
+ if(number % 5 == 0) {
+ print("\(number) is divisible by 5")
+ }
+}
+for number in 0..<10 {
+ print(number)
+}
+for index in 0..<stupidIdeas.count {
+ print(stupidIdeas[index])
+}
+var stuff = ["First thing", "Second thing", "Third thing", "Fourth thing",
+"Fifth thing"]
+while stuff.count > 0 {
+ print(stuff)
+ stuff.removeLast()
+}
+var number = 5
+while number > 0 {
+ print(number)
+ number -= 1
+}
