e9afcdde24a7adb102d7111354728aadb1fbba9c
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-10-10 09:43:12 -0500

    modified:   Unit 5/Three Letter Word/Three Letter Word/ContentView.swift

--- Unit 5/Three Letter Word/Three Letter Word/ContentView.swift
+++ Unit 5/Three Letter Word/Three Letter Word/ContentView.swift
@@ -1,21 +1,56 @@
 //
 //  ContentView.swift
-//  Three Letter Word
+//  Tic Tac Toe
 //
-//  Created by Raphael Abano on 10/10/24.
+//  Created by Raphael Abano on 10/9/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var letter = ""
+    @State private var counter = 0
+    @State private var selectedLetter = ["", "", ""]
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Three Letter Word")
+                .font(.title).bold()
+                .padding()
+            Text("Tap the gray box to change the letter")
+            CustomLetterBox(color: .gray, text: letter)
+                .onTapGesture {
+                    let position = alphabet.index(alphabet.startIndex, offsetBy: counter)
+                    letter = String(alphabet[position])
+                    counter += 1
+                    if counter == alphabet.count {
+                        counter = 0
+                    }
+                }
+            Text("Tap a red box to select that letter")
+            HStack {
+                ForEach(0..<3) {index in
+                    CustomLetterBox(color: .red, text: selectedLetter[index])
+                        .onTapGesture {
+                            selectedLetter[index] = letter
+                        }
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct CustomLetterBox: View {
+    let color : Color
+    let text : String
+    var body: some View {
+        ZStack {
+            color
+            Text(text)
+                .font(.system(size: 90))
+                .fontWeight(.heavy)
+        }
+        .frame(width: 120, height: 120)
     }
 }
 
